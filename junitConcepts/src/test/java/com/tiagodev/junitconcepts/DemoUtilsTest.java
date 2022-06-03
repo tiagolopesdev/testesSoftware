23ea98d6120ff848efd4446fa1616369d847fcc6
tiagoLopes <saxtiago14@gmail.com>
2022-06-02 22:08:56 -0300

    Finalizado, JUnit assertions Same/NotSame e Tru/False.

--- junitConcepts/src/test/java/com/tiagodev/junitconcepts/DemoUtilsTest.java
+++ junitConcepts/src/test/java/com/tiagodev/junitconcepts/DemoUtilsTest.java
@@ -4,12 +4,9 @@
  */
 package com.tiagodev.junitconcepts;
 
-import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -22,27 +19,43 @@ class DemoUtilsTest {
 
     @BeforeEach
     void setupBeforeEach() {
-        demoUtils = new DemoUtils();        
+        demoUtils = new DemoUtils();
     }
 
-//    @AfterEach
-//    void setupAfterEach() {        
-//    }
-
     @Test
-    @DisplayName("Test equals and not equals")
-    void testEqualsAndNotEquals() {        
+    @DisplayName("Equals and not equals")
+    void testEqualsAndNotEquals() {
         assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
     }
 
     @Test
-    @DisplayName("Test null and not null")
-    void testNullAndNotNull() {        
+    @DisplayName("Null and not null")
+    void testNullAndNotNull() {
         String strOne = null;
         String strTwo = "Tiago";
         assertNull(demoUtils.checkNull(strOne), "object should be null");
         assertNotNull(demoUtils.checkNull(strTwo), "object should not be null");
+    }
+    
+    @Test
+    @DisplayName("Same and not same")
+    void testSameAndNotSame(){
+        String str = "luv2code";
+        
+        assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(),
+                "Objects should refer to same object");
+        assertNotSame(str, demoUtils.getAcademy(), "Objects should not refer"
+                + "to same object");
+    }
+    
+    @Test
+    @DisplayName("True and false")
+    void testTrueFalse(){
+        int a = 10;
+        int b = 5;
+        assertTrue(demoUtils.isGreater(a, b), "This should return true");
+        assertNotNull(demoUtils.isGreater(b, a), "This should return true");
     }
 
 }
