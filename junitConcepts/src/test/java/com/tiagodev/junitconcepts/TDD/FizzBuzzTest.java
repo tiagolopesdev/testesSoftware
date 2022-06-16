ba81b2efed5e6f4b15f04083f2e776cd81cfe68e
Tiago Lopes <saxtiago14@gmail.com>
2022-06-15 21:02:49 -0300

    Ordenando os métodos da classe.

--- junitConcepts/src/test/java/com/tiagodev/junitconcepts/TDD/FizzBuzzTest.java
+++ junitConcepts/src/test/java/com/tiagodev/junitconcepts/TDD/FizzBuzzTest.java
@@ -3,8 +3,13 @@ package com.tiagodev.junitconcepts.TDD;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DisplayName("FizzBuzz test")
 public class FizzBuzzTest {
 
     FizzBuzz fizzBuzz;
@@ -15,6 +20,7 @@ public class FizzBuzzTest {
     }
 
     @Test
+    @Order(1)
     @DisplayName("Divisivel por 3")
     void testForDivisibleByThree() {
         String expected = "Fizz";
@@ -23,6 +29,7 @@ public class FizzBuzzTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("Divisivel por 5")
     void testForDivisivelByFive() {
         String expected = "Buzz";
@@ -31,18 +38,19 @@ public class FizzBuzzTest {
     }
 
     @Test
+    @Order(3)
     @DisplayName("Divisivel por 5 e 3")
     void testForDivisibleByThreeAndFive() {
         String expected = "FizzBuzz";
         Assertions.assertEquals(expected, fizzBuzz.divisible(15),
                 "Número divisivel por 3 e 5");
     }
-    
+
     @Test
+    @Order(4)
     @DisplayName("Not Divisible by three or five")
-    void testNotDivisibleByThreeOrFive(){
-        String expected = "1";        
+    void testNotDivisibleByThreeOrFive() {
+        String expected = "1";
         Assertions.assertEquals(expected, fizzBuzz.divisible(1), "Return number");
     }
-
 }
