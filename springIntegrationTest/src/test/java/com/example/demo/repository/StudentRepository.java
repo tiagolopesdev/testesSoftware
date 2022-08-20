b2cff3e0243c2e088e7d061e14fdeae23cf99824
Tiago Lopes <saxtiago14@gmail.com>
2022-08-19 22:57:24 -0300

    Retirando repository da camada de testes.

--- springIntegrationTest/src/test/java/com/example/demo/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.models.CollegeStudent;
-
-public interface StudentRepository {
-
-	CollegeStudent findByEmailAddress(String string);
-	
-}
